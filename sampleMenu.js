d5bd6776023de480b71cd08d775e6bf0500db50b
low_zt <monkry81611@gmail.com>
2022-08-14 10:35:20 +0800

    change vidio play2

--- sampleMenu.js
+++ sampleMenu.js
@@ -3,3 +3,19 @@ function sampleMenu(item) {
   elem.innerHTML = item.innerText;
   event.preventDefault();
 }
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'M7lc1UVf-VE',
+    playerVars: {
+      'playsinline': 1
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
